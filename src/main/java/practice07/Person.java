db11752b5dc254d1dfd94c4ef715aeb00dfef38c
GAOGA4 <961977096@qq.com>
2020-07-22 17:48:01 +0800

    [Gavin] feature: finish practice7

--- src/main/java/practice07/Person.java
+++ src/main/java/practice07/Person.java
@@ -1,4 +1,25 @@
 package practice07;
 
 public class Person {
+    protected String name;
+    protected int age;
+
+    public Person(String name,int age) {
+        this.name = name;
+        this.age=age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+
+    public String introduce(){
+        return String.format("My name is %s. I am %s years old.",this.name,this.age);
+    }
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,20 @@
 package practice07;
 
-public class Student {
+
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(String name, int age,Klass klass) {
+        super(name, age);
+        this.klass=klass;
+    }
+
+
+    public Klass getKlass() {
+        return klass;
+    }
+    public String introduce(){
+        return  String.format("My name is %s. I am %s years old. I am a Student. I am at Class %s.",this.name,this.age,this.klass);
+    }
 }
+
