decc27f8f630757acca4a18fb7d0aa6fadd3c585
96197 <961977096@qq.com>
2020-07-22 21:46:22 +0800

    [Gavin] feature:complete practice9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,34 @@
 package practice09;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %s",this.number);
+    }
+
+    public void assignLeader(Student student){
+        if(student.getKlass().number!=this.number){
+            System.out.print("It is not one of us.\n");
+        }else{
+            student.getKlass().leader=student;
+        }
+
+    }
+    public void appendMember(Student student){
+        student.setKlass(this);
+    }
 }
