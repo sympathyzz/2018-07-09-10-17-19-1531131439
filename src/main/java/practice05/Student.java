6091e430ff1173f202d6cdf480c9e440fa503e0b
GAOGA4 <961977096@qq.com>
2020-07-22 16:18:13 +0800

    [Gavin] feature: finish practice5

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,21 @@
 package practice05;
 
-public class Student {
+
+public class Student extends Person {
+    private int klass;
+
+    public Student(String name, int age,int klass) {
+        super(name, age);
+        this.klass=klass;
+    }
+
+
+    public int getKlass() {
+        return klass;
+    }
+    public String introduce(){
+        return "My name is "+this.name+". I am "+this.age+" years old. I am a Student. I am at Class "+this.klass+".";
+    }
 }
+
+
--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,11 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce(){
+        return "My name is "+this.name+". I am "+this.age+" years old. I am a Worker. I have a job.";
+    }
 }
