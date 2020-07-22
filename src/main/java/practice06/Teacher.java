919a26e76869ecb0e716e67b74fedc348133ab0d
GAOGA4 <961977096@qq.com>
2020-07-22 16:35:54 +0800

    [Gavin] feature: finish practice6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,27 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+
+    public Teacher(String name, int age,int klass) {
+        super(name, age);
+        this.klass=klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+    public String introduce(){
+        if(this.klass!=0){
+            return "My name is "+this.name+". I am "+this.age+" years old. I am a Teacher. I teach Class "+this.klass+".";
+        }else{
+            return "My name is "+this.name+". I am "+this.age+" years old. I am a Teacher. I teach No Class.";
+        }
+
+    }
 }
