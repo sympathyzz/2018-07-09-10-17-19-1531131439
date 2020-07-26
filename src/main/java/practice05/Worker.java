994d04f11deacad55a38d1e0e394dcaa5896cc90
96197 <961977096@qq.com>
2020-07-26 15:35:06 +0800

    [practice5][Gavin] update:update practice5

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -6,6 +6,6 @@ public class Worker extends Person{
     }
 
     public String introduce(){
-        return "My name is "+this.name+". I am "+this.age+" years old. I am a Worker. I have a job.";
+        return super.introduce()+" I am a Worker. I have a job.";
     }
 }
